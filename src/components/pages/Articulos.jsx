b44ad8cbcdccc773e7b3fd0072585382a77fd44a
Meli Barrios <melissa.blecona@gmail.com>
2023-12-12 20:27:30 -0600

    se agrega icono, info useAjax, useForms, se avanza en codigo de componente articulos

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Global } from "../../helpers/Global";
 
 const Articulos = () => {
   const [articulos, setArticulos] = useState([]);
@@ -8,7 +9,7 @@ const Articulos = () => {
   }, []);
 
 const conseguirArticulos = async() => {
-  const url= "http://localhost:3900/api/articulos"
+  const url= Global.url+"articulos"
   let peticion= await fetch(url, {
     method:"GET"
   })
@@ -16,28 +17,32 @@ const conseguirArticulos = async() => {
    if(datos.status === "success"){
     setArticulos(datos.articulos)
   } 
-  console.log(datos);
+  //console.log(datos);
 }
   return (
     <>
    
       {
-      
-      articulos.map((articulo => {
-        return (
-          <article key={articulo._id} className="articulo-item">
-          <div className="mascara">
-            <img src="https://cdn.pixabay.com/photo/2023/11/20/23/11/woman-8402067_1280.jpg" alt=""/>
-          </div>
-          <div className="datos">
-            <h3 className="title">{articulo.titulo}</h3>
-            <p className="description">{articulo.contenido}</p>
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-          </div>
-        </article>
-        );
-      }))}
+      articulos.length >= 1 ?(
+
+        articulos.map(articulo => {
+          return (
+            <article key={articulo._id} className="articulo-item">
+            <div className="mascara">
+              <img src="https://cdn.pixabay.com/photo/2023/11/20/23/11/woman-8402067_1280.jpg" alt=""/>
+            </div>
+            <div className="datos">
+              <h3 className="title">{articulo.titulo}</h3>
+              <p className="description">{articulo.contenido}</p>
+              <button className="edit">Editar</button>
+              <button className="delete">Borrar</button>
+            </div>
+          </article>
+          );
+        })
+      )
+      : <h1>no hay articulos</h1>
+      }
     </>
   );
 };
